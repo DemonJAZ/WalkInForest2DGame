0c4576f70590f1f075148170219485bf15515086
DemonJAZ <jpjaspreetmahal@gmail.com>
2019-06-08 15:12:37 +0530

    starting scene
    Need to figure out kinematics in 2d games

---
+++ WalkInForest2DGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool grounded = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime);
+        }
+        if (!grounded)
+        {
+            transform.Translate(Vector2.down * 3f * Time.deltaTime);
+        }
+
+        if (!Physics2D.Raycast(gameObject.transform.position, Vector2.down, 3f))
+        {
+            grounded = true;
+        }
+    }
+
+
+}
