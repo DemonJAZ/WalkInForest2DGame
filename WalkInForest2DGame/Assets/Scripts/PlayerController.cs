cecec074fc19eed7e0431de5ae220fdfed1f1136
DemonJAZ <jpjaspreetmahal@gmail.com>
2019-06-09 19:21:40 +0530

    Movements done
    MOvements Improved , jump implemented successfully.
    change position is the key in each fream , no velocity or acceleration in kinemaics. https://answers.unity.com/questions/1639100/jumping-isnt-smooth-while-going-up.html?childToView=1639144

--- WalkInForest2DGame/Assets/Scripts/PlayerController.cs
+++ WalkInForest2DGame/Assets/Scripts/PlayerController.cs
@@ -5,47 +5,58 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private bool grounded = false;
-    public float jumpforce;
+    public float gravitymodifier = 1f;
+    public float jumpspeed = 1f;
     public float movementSpeed;
+    private Vector2 Velocity;
     private Rigidbody2D Pico;
-    private float inputX,inputY;
 
     // Start is called before the first frame update
     void Start()
     {
         Pico = gameObject.GetComponent<Rigidbody2D>();
     }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Velocity.y = jumpspeed * Time.deltaTime;
+            transform.Translate(Velocity);
+            grounded = false;
 
+        }
+    }
     // Update is called once per frame
     private void FixedUpdate()
     {
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+            Velocity.x = movementSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(Vector2.left * movementSpeed * Time.deltaTime);
+            Velocity.x = -movementSpeed * Time.deltaTime;
         }
+        else
+        {
+            Velocity.x = 0.0f; //to set velocity to zero when no key is pressed.
+        }
+
         if (!grounded)
         {
-            transform.Translate(Physics2D.gravity*Time.deltaTime);
+
+            Velocity.y += (Physics2D.gravity.y * gravitymodifier * Time.deltaTime);
         }
 
-        if (Physics2D.Raycast((Vector2)transform.position + (Vector2.down * 0.7f) , Vector2.down,0.1f) && !grounded)
-        { 
-            grounded = true;
-        }
-        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+
+
+        if (Physics2D.Raycast((Vector2)transform.position + (Vector2.down * 0.7f), Vector2.down, 0.1f) && !grounded && Velocity.y<=0)
         {
-            Pico.isKinematic = false;
-            Pico.AddForce(Vector2.up * jumpforce);
-            Pico.isKinematic = true;
-            grounded = false;
-
+            grounded = true;
+            Velocity.y = 0.0f; // to stop speed in vertical direction.
         }
 
-
+        transform.Translate(Velocity);
     }
 
 
